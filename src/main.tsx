d28e45458ffc81cd57008eb8dd69fdc96d44cb1f
Kelman Santos <kelman.santos@tsco.co.ao>
2025-02-17 16:22:01 +0100

    Helmet Init

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <HelmetProvider>
+      <Helmet>
+        <title>Portfolio - Kelman Santos</title>
+        <meta name="description" content="Kelman Santos personal website "/>
+      </Helmet>
+    </HelmetProvider>
     <App />
   </React.StrictMode>,
 )
